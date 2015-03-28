5f3a53628eed5f90ae88f33e7a54be6c68629f73
akhileshsrikanth <akhilesh.srikanth@gmail.com>
2015-03-29 00:58:21 +0530

    brute force solution

---
+++ src/yass/solve.clj
@@ -0,0 +1,24 @@
+(ns yass.solve
+  (:use [clojure.set]))
+
+(defn constraints
+  "Returns a set of numbers which can't be filled at the given position"
+  [s i]
+  (let [gridList (partition 9 s),       ;; create a list of lists
+        row (/ i 9),                    ;; row no
+        column (mod i 9),               ;; column no
+        columnGroup (/ column 3),            ;; column group no
+        rowGroup (/ row 3),               ;; row group no
+        every-nth-item (fn [gridList i] (map #(nth % i) gridList))                ;; return every nth item in the list of lists.
+        group-column (every-nth-item (map #(partition 3 %) gridList) columnGroup) ;; returns all nos. in the column group
+        group (take 3 (drop (* 3 (int rowGroup)) group-column))]                  ;; returns all nos. in the given number's group.
+    (into #{} (flatten [(nth gridList row) (every-nth-item gridList column) group])))) ;; finally return the constaint numbers
+
+(defn solve
+  [sudokuGrid]
+  (if (.contains sudokuGrid 0)        ;; check if the grid is solved
+    (let [i (.indexOf sudokuGrid 0)   ;; index of the first blank no in the grid (0)
+          inject #(concat (take %2 %1) [%3] (drop (inc %2) %1))]    ;; inject values on a per-value basis (each value belongs to the unconstrained set
+      (flatten (map #(solve (inject sudokuGrid i %))
+                    (difference #{1 2 3 4 5 6 7 8 9} (constraints sudokuGrid i)))))     ;; check each injected value and see if the board is solved.
+    sudokuGrid))
