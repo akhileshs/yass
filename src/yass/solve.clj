638512dbff70f48a474e39f7bd0e65aebf122899
akhileshsrikanth <akhilesh.srikanth@gmail.com>
2015-03-30 14:52:13 +0530

    fixed typings

--- src/yass/solve.clj
+++ src/yass/solve.clj
@@ -1,23 +1,23 @@
 (ns yass.solve
-  (:use [clojure.set]
-        [clojure.core.typed]))
+  (:use [clojure.set])
+  (:require [clojure.core.typed :as t]))
 
+(t/ann constraints [(t/Seq Number) Number -> (t/Set Number)])
 (defn constraints
   "Returns a set of numbers which can't be filled at the given position"
-  [s :- (Vec Number), i :- AnyInteger]
-  (let [gridList :- (Seq (Seq Number)) (partition 9 s),       ;; create a list of lists
-        row :- Number (/ i 9),                    ;; row no
-        column :- Number (mod i 9),               ;; column no
-        columnGroup :- Number (/ column 3),            ;; column group no
-        rowGroup :- Number (/ row 3),               ;; row group no
-        every-nth-item (fn [gridList :- (Seq (Seq Number)), i :- Number] (map #(nth % i) gridList))            ;; return every nth item in the list of lists.
-
-        group-column :- (Seq Number) (every-nth-item (map #(partition 3 %) gridList) columnGroup) ;; returns all nos. in the column group
-        group :- (Seq Number) (take 3 (drop (* 3 (int rowGroup)) group-column))]                  ;; returns all nos. in the given number's group.
-    (into #{} (flatten [(nth gridList row) (every-nth-item gridList column) group])))) ;; finally return the constaint numbers
+  [s i]
+  (let [gridList (t/ann-form (partition 9 s) (t/Seq (t/Seq Number))),       ;; create a list of lists
+        row (t/ann-form (/ i 9) Number),                    ;; row no
+        column (t/ann-form (mod i 9) Number),               ;; column no
+        columnGroup (t/ann-form (/ column 3) Number),            ;; column group no
+        rowGroup (t/ann-form (/ row 3) Number),               ;; row group no
+        every-nth-item (fn [gridList i] (map #(nth % i) gridList))   ;; return every nth item in the list of lists.
+        group-column (t/ann-form (every-nth-item (map #(partition 3 %) gridList) columnGroup) (t/Seq Number)) ;; returns all nos. in the column group
+        group (t/ann-form (take 3 (drop (* 3 (int rowGroup)) group-column)) (t/Seq (t/Seq Number)))]                  ;; returns all nos. in the given number's group.
+     (into #{} (flatten [(nth gridList row) (every-nth-item gridList column) group])))) ;; finally return the constaint numbers
 
 (defn solve
-  [sudokuGrid :- (Vec Number)]
+  [sudokuGrid]
   (if (.contains sudokuGrid 0)        ;; check if the grid is solved
     (let [i (.indexOf sudokuGrid 0)   ;; index of the first blank no in the grid (0)
           inject #(concat (take %2 %1) [%3] (drop (inc %2) %1))]    ;; inject values on a per-value basis (each value belongs to the unconstrained set
